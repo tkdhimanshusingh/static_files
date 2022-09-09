6ff38418debb455275aaaea0426aa17b846984dd
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-09 18:56:04 +0530

    update

--- course/views.py
+++ course/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def learndj(request):
+    return render(request,'course/courseone.html',{'title':'Learn Django','cname':'Django'})
--- fees/views.py
+++ fees/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
+def feesdj(request):
+    return render(request,'fees/feesone.html',{'title':'Fees Django','cname':'Django','charge':300})
